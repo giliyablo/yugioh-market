5e3af0ff2d0156d5a19bf5735f04ab88a055147a
giliyablo <gili.yablonka@gmail.com>
2025-10-15 03:17:33 +0300

    Better worker

--- worker/jobs.js
+++ worker/jobs.js
@@ -7,9 +7,9 @@ const processJob = async (db, jobData) => {
     }
 
     const postRef = db.collection('posts').doc(postId);
-    const [priceResult, imageUrl] = await Promise.all([
-        getMarketPrice(cardName),
-        getCardImageFromYugipedia(cardName)
+    const [imageUrl, priceResult] = await Promise.all([
+        getCardImageFromYugipedia(cardName),
+        getMarketPrice(cardName)
     ]);
 
     const updatePayload = {};
@@ -23,6 +23,9 @@ const processJob = async (db, jobData) => {
 
     if (Object.keys(updatePayload).length > 0) {
         await postRef.update(updatePayload);
+    } else {
+        // If no data was fetched, throw an error to mark the job as failed
+        throw new Error('Failed to fetch both price or image for the card.');
     }
 };
 
